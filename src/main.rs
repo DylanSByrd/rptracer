337a74caaace83ee49827842316b98e73f65dfcb
DylanSByrd <mail@dylansbyrd.com>
2018-09-16 12:17:40 -0700

    Chapter 3 - rays, camera, background

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 extern crate image;
 extern crate vecmat;
 
+use vecmat::vec::*;
+
+mod ray;
+use ray::Ray;
+
 use std::io::Write;
 use std::str::FromStr;
 
@@ -50,21 +55,37 @@ fn parse_args() -> Config {
     }
 }
 
+fn get_color_for_ray(ray: &Ray) -> Vec3<f64> {
+    let unit_dir = ray.dir.normalize();
+    let time = 0.5 * (unit_dir[1]) + 1.0;
+
+    (1.0 - time) * Vec3::<f64>::from(1.0, 1.0, 1.0) + time * Vec3::<f64>::from(0.5, 0.7, 1.0)
+}
+
+
 fn main() {
     let config = parse_args();
 
     let mut pixel_buf: Vec<u8> = vec![0; config.dimx * config.dimy * 3];
 
+    let lower_left = Vec3::<f64>::from(-2.0, -1.0, -1.0);
+    let horizontal = Vec3::<f64>::from(4.0, 0.0, 0.0);
+    let vertical = Vec3::<f64>::from(0.0, 2.0, 0.0);
+    let origin = Vec3::<f64>::from(0.0, 0.0, 0.0);
+
     let mut pixel_index = 0;
     for j in 0..config.dimy {
         for i in 0..config.dimx {
-            let fr: f64 = i as f64 / config.dimx as f64;
-            let fg: f64 = (config.dimy - j) as f64 / config.dimy as f64;
-            let fb: f64 = 0.2;
+            let u = i as f64 / config.dimx as f64;
+            let v = (config.dimy - j) as f64 / config.dimy as f64;
+
+            let dir = lower_left + u * horizontal + v * vertical;
+            let ray = Ray::from_point_and_dir(origin, dir);
+            let color = get_color_for_ray(&ray);
             
-            let ir = (255.99 * fr) as u8;
-            let ig = (255.99 * fg) as u8;
-            let ib = (255.99 * fb) as u8;
+            let ir = (255.99 * color[0]) as u8;
+            let ig = (255.99 * color[1]) as u8;
+            let ib = (255.99 * color[2]) as u8;
             
             pixel_buf[pixel_index] = ir;
             pixel_index += 1;
--- src/ray.rs
+++ src/ray.rs
@@ -2,27 +2,27 @@ extern crate vecmat;
 
 use vecmat::vec::*;
 
-struct Ray {
-    origin: Vec3<f64>,
-    dir: Vec3<f64>,
+pub struct Ray {
+    pub origin: Vec3<f64>,
+    pub dir: Vec3<f64>,
 }
 
 impl Ray {
-    fn new() -> Ray {
+    pub fn new() -> Ray {
         Ray {
             origin: Vec3::<f64>::new(),
             dir: Vec3::<f64>::new(),
         }
     }
 
-    fn from_point_and_dir(origin: Vec3<f64>, dir: Vec3<f64>) -> Ray {
+    pub fn from_point_and_dir(origin: Vec3<f64>, dir: Vec3<f64>) -> Ray {
         Ray {
-            orign,
+            origin,
             dir,
         }
     }
 
-    fn get_point_at_time(&self, time: f64) -> Vec3<f64> {
-        origin + (time * dir)
+    pub fn get_point_at_time(&self, time: f64) -> Vec3<f64> {
+        self.origin + (time * self.dir)
     }
 }
