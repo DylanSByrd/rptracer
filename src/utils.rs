55fa2cd0647634e2befe9060eeb85700ac762b4b
DylanSByrd <mail@dylansbyrd.com>
2018-09-20 08:42:59 -0700

    Fix bug in get_point_in_unit_sphere

--- src/utils.rs
+++ src/utils.rs
@@ -2,15 +2,12 @@ use vecmat::vec::Vec3;
 use rand::{self, Rng};
 
 pub fn get_point_in_unit_sphere() -> Vec3<f64> {
-    // Rejection method
-    // #TODO - I don't care for it; should replace
     let mut rng = rand::thread_rng();
-
     loop {
         let point = 2.0 * Vec3::<f64>::from(rng.gen::<f64>(), rng.gen::<f64>(), rng.gen::<f64>())
             - Vec3::<f64>::from(1.0, 1.0, 1.0);
 
-        if point.sqrlen() >= 1.0 {
+        if point.sqrlen() <= 1.0 {
             return point;
         }
     }
