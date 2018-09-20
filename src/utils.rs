c8aa8310c77f3b122ccd94451e03fa20bc7dbd9e
DylanSByrd <mail@dylansbyrd.com>
2018-09-20 09:23:08 -0700

    Another bugfix and cleanup

--- src/utils.rs
+++ src/utils.rs
@@ -4,10 +4,10 @@ use rand::{self, Rng};
 pub fn get_point_in_unit_sphere() -> Vec3<f64> {
     let mut rng = rand::thread_rng();
     loop {
-        let point = 2.0 * Vec3::<f64>::from(rng.gen::<f64>(), rng.gen::<f64>(), rng.gen::<f64>())
+        let point = 2.0 * Vec3::<f64>::from(rng.gen(), rng.gen(), rng.gen())
             - Vec3::<f64>::from(1.0, 1.0, 1.0);
 
-        if point.sqrlen() <= 1.0 {
+        if point.sqrlen() < 1.0 {
             return point;
         }
     }
